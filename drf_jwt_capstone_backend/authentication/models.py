d45dbd872c3f086b08fafcfbb4ff37a2b8d403b9
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-08 17:15:40 -0600

    Chore: Add columns to User table

--- drf_jwt_capstone_backend/authentication/models.py
+++ drf_jwt_capstone_backend/authentication/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 
 # To add new columns to the authentication_user table include the properties
 # in the model below
+
 # In order for the new columns to appear in the database run:
 # 1. python manage.py makemigrations
 # 2. python manage.py migrate
@@ -18,3 +19,6 @@ class User(AbstractUser):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=15)
     zip_code = models.IntegerField()
+    is_active = models.CharField(max_length=3)
+    balance = models.IntegerField() 
+    
