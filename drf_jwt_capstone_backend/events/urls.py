42f82bf9cb5b23dc09a8df6cb7a15430e1a66a59
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-09 16:29:25 -0600

    perf: Remove unnecessary code

--- drf_jwt_capstone_backend/events/urls.py
+++ drf_jwt_capstone_backend/events/urls.py
@@ -2,6 +2,7 @@
 # 127.0.0.1:8000/           events/                             add_event/
 # localsettings.py          drf_jwt_capstone_backen urls.py     events urls.py 
 
+
 from django.contrib import admin 
 from django.urls import path 
 from events import views 
