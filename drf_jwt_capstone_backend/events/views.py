33077cfd16923942cefc062a61084767e38487bc
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-09 09:34:06 -0600

    Chore: Add necessary imports to Views

--- drf_jwt_capstone_backend/events/views.py
+++ drf_jwt_capstone_backend/events/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth import get_user_model
+from .serializers import EventSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+User = get_user_model()
+
 
 # Create your views here.
+# Admin/Events 
+# create_event
+# update_event
+# delete_event 
+# view_events 
+
+
+# User/Events
+# view_events
+# rsvp_events
+# view_rsvp_map
--- drf_jwt_capstone_backend/members/views.py
+++ drf_jwt_capstone_backend/members/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth import get_user_model
+from .serializers import MemberSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+User = get_user_model()
 
 # Create your views here.
