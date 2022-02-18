5ac7e35c5221075b319c1085009b5cd83bdaf402
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-17 20:19:36 -0600

    Chore: add/remove print line for testing

--- drf_jwt_capstone_backend/manage.py
+++ drf_jwt_capstone_backend/manage.py
@@ -6,7 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    print("----------------------------in main")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                           'drf_jwt_capstone_backend.settings')
     try:
